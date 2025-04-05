ffda1b7504438de2fc41edb155abf2470179aa75
aysuyigitt <79106302+aysuyigitt@users.noreply.github.com>
2025-04-05 15:51:02 +0300

    SingletonDBConnection

--- src/main/java/com/aysuyigit/yonetim_uygulamasi_javafx/dao/IDaoImplements.java
+++ src/main/java/com/aysuyigit/yonetim_uygulamasi_javafx/dao/IDaoImplements.java
@@ -1,7 +1,7 @@
 package com.aysuyigit.yonetim_uygulamasi_javafx.dao;
 
 
-//import java.sql.Connection;
+import java.sql.Connection;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,24 +9,26 @@ import java.util.Optional;
 public interface IDaoImplements <T> {
 
     //CREATE
-    Optional<T>create(T t);
+    Optional<T> create(T t);
 
     //List
-    List<T>list();
+    List<T> list();
 
     //FIND
-    Optional<T>findByName(String name);
-    Optional<T>findById(int id);
+    Optional<T> findByName(String name);
+
+    Optional<T> findById(int id);
 
     //UPDATE
-    Optional<T>update(int id, T entity);
+    Optional<T> update(int id, T entity);
 
     //DELETE
-    Optional<T>delete(int id);
+    Optional<T> delete(int id);
 
 
-    //default Connection iDaoImplementsDatabaseConnection(){
-        // null;
-    //}
+    default Connection iDaoImplementsDatabaseConnection() {
+         return null;
+    }
 
 }
+
