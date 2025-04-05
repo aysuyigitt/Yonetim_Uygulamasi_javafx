a77477743545d500a956e504f73734f34c5eac3b
aysuyigitt <79106302+aysuyigitt@users.noreply.github.com>
2025-04-05 17:31:02 +0300

    SingletonDBConnection, H2DB

--- src/main/java/com/aysuyigit/yonetim_uygulamasi_javafx/database/SingletonDBConnection.java
+++ src/main/java/com/aysuyigit/yonetim_uygulamasi_javafx/database/SingletonDBConnection.java
@@ -7,28 +7,52 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class SingletonDBConnection {
 
-    private static final String URL = "jdbc:h2./h2db/blog" + "AUTO_SERVER=TRUE";
+    //private static final String URL = "jdbc:h2./h2db/user_managament" + "AUTO_SERVER=TRUE";
+    private static final String URL = "jdbc:h2:~/h2db/user_managament";
     private static final String USERNAME = "sa";
     private static final String PASSWORD = "";
 
-    private SingletonDBConnection() {
-
-    }
-
+    private static SingletonDBConnection instance;
     private static Connection connection;
 
-    public static Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            try {
-                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-                System.out.println(SpecialColor.GREEN + "Veritabanı bağlantısı başarılı" + SpecialColor.RESET);
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
-                System.out.println(SpecialColor.RED + "Veritabanı bağlantısı başarısız" + SpecialColor.RESET);
-                throw new RuntimeException("Veritabanı bağlantısı başarısız");
-
-            }
+    private SingletonDBConnection() {
+        try {
+            Class.forName("org.h2.Driver");
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println(SpecialColor.GREEN + "Veritabanı bağlantısı başarılı" + SpecialColor.RESET);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.out.println(SpecialColor.RED + "Veritabanı bağlantısı başarısız" + SpecialColor.RESET);
+            throw new RuntimeException("Veritabanı bağlantısı başarısız");
         }
+    }
+
+    public static synchronized SingletonDBConnection getInstance(){
+        if(instance==null){
+            instance= new SingletonDBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
         return connection;
     }
+
+    //Database kapatmak
+    public static  void closeConnection(){
+        if(instance!=null&&instance.connection!=null){
+            try{
+                instance.connection.close();
+                System.out.println(SpecialColor.RED + "Veritabanı bağlantısı kapatıldı");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
+
+
+
+
+
+
