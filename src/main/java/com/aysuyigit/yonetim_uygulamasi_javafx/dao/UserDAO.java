a7ab588f441af9191508e7d3d010a8528ee798a5
aysuyigitt <79106302+aysuyigitt@users.noreply.github.com>
2025-04-06 11:28:01 +0300

    CRUD-CREATE-LİST

--- src/main/java/com/aysuyigit/yonetim_uygulamasi_javafx/dao/UserDAO.java
+++ src/main/java/com/aysuyigit/yonetim_uygulamasi_javafx/dao/UserDAO.java
@@ -5,6 +5,9 @@ import com.aysuyigit.yonetim_uygulamasi_javafx.dto.UserDTO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +31,19 @@ public class UserDAO implements IDaoImplements<UserDTO> {
             preparedStatement.setString(2, userDTO.getPassword());
             preparedStatement.setString(3, userDTO.getEmail());
 
+            int affectedRows= preparedStatement.executeUpdate();
+
+            if(affectedRows>0){
+                try(ResultSet generatedKeys= preparedStatement.getGeneratedKeys()){
+                    if(generatedKeys.next()){
+                        userDTO.setId((generatedKeys.getInt((1))));
+                        return Optional.of(userDTO);
+                    }
+                }catch(SQLException sqlException){
+                    sqlException.printStackTrace();
+
+                }
+            }
         }catch(Exception exception){
             exception.printStackTrace();
         }
@@ -35,8 +51,25 @@ public class UserDAO implements IDaoImplements<UserDTO> {
     }
 
     @Override
-    public List<UserDTO> list() {
-        return List.of();
+    public Optional<List<UserDTO>>list() {
+        List<UserDTO> userDTOList = new ArrayList<>();
+        String sql = "SELECT*FROM users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery(sql);
+
+            while(resultSet.next()){
+                userDTOList.add(UserDTO.builder()
+                        .id(resultSet.getInt("id"))
+                        .username(resultSet.getString("username"))
+                        .password(resultSet.getString("password"))
+                        .email(resultSet.getString("email"))
+                        .build());
+            }
+            return userDTOList.isEmpty()? Optional.empty() : Optional.of(userDTOList);
+    }catch(Exception exception){
+            exception.printStackTrace();
+        }
+        return Optional.empty();
     }
 
     @Override
